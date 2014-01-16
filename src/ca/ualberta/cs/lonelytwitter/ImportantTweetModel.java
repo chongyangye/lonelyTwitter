522ed792547461e0bd4e84defb3ef6fa2d953dcd
chongyangye <cye2@ualberta.ca>
2014-01-16 15:21:17 -0700

    Abstracting the class

---
+++ src/ca/ualberta/cs/lonelytwitter/ImportantTweetModel.java
@@ -0,0 +1,25 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class ImportantTweetModel extends LonelyTweetModel {
+
+	@Override
+	public Date getTimestamp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ImportantTweetModel(String text, Date timestamp) {
+		super(text, timestamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImportantTweetModel(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+	public String getText(){
+		return "Important! " + super.getText();
+	}
+}
