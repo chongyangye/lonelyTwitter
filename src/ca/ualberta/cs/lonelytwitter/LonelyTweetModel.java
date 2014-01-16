fe6b8147f00ee6084b619ca04a6dedc923b7c915
chongyangye <cye2@ualberta.ca>
2014-01-16 14:57:38 -0700

    Constructors

--- src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -8,14 +8,30 @@ public class LonelyTweetModel {
 	public String getText() {
 		return text;
 	}
+	
+	public LonelyTweetModel(String text){
+		super();
+		this.text=text;
+		timestamp=new Date();
+	}
+	
+	public LonelyTweetModel(String text, Date timestamp){
+		super();
+		this.text=text;
+		this.timestamp=timestamp;
+	}
+	
 	public void setText(String text) {
 		this.text = text;
 	}
+	
 	public Date getTimestamp() {
 		return timestamp;
 	}
+	
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
+
 	
 }
